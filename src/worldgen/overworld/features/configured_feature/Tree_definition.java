9ef249a38d8cfe065d61f53ad83bd523b4711b2b
sakura100mochi <Yuina.o0h731@gmail.com>
2026-06-03 21:40:38 +0200

    [feature] shape of leaves bush_foliage_placer

--- src/worldgen/overworld/features/configured_feature/Tree_definition.java
+++ src/worldgen/overworld/features/configured_feature/Tree_definition.java
@@ -98,6 +98,19 @@ public final class Tree_definition {
 				}
 				break;
 			case "minecraft:bush_foliage_placer":
+				height = Provider.getIntProvider(foliage_placer.get("height"), this.data.random);
+				offset = Provider.getIntProvider(foliage_placer.get("offset"), this.data.random);
+				radius = Provider.getIntProvider(foliage_placer.get("radius"), this.data.random);
+				for (int y_pos = y + trunk_height + offset; y_pos >= y + trunk_height + offset - height + 1; y_pos--) {
+					for (int x_pos = x - radius; x_pos <= x + radius; x_pos++) {
+						for (int z_pos = z - radius; z_pos <= z + radius; z_pos++) {
+							BlockState foliage = BlockStateProvider.getBlockState(this.data, foliage_provider, x_pos, y_pos, z_pos);
+							int id = Registry.getId(foliage.identifier);
+							result.add(new Configured_featureInfo(x_pos, y_pos, z_pos, id, true));
+						}
+					}
+					radius++;
+				}
 				break;
 			case "minecraft:fancy_foliage_placer":
 				height = Provider.getIntProvider(foliage_placer.get("height"), this.data.random);
