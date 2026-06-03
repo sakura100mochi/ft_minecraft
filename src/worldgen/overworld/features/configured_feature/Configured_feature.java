4ef7de7c36882b65c77ce86039255c293a4b2b9c
sakura100mochi <Yuina.o0h731@gmail.com>
2026-06-03 21:01:10 +0200

    [feature] add ore

--- src/worldgen/overworld/features/configured_feature/Configured_feature.java
+++ src/worldgen/overworld/features/configured_feature/Configured_feature.java
@@ -21,6 +21,7 @@ public final class Configured_feature {
 	private final Data				data;
 	private final Placed_feature	placed_feature;
 	private final Tree_definition	tree_definition;
+	private final OreFeature		oreFeature;
 	private final Map<String, IConfigured_featureInfo> configured_feature = new HashMap<>();
 	private final int min_y;
 	private final int dirtId;
@@ -30,6 +31,7 @@ public final class Configured_feature {
 		this.data = data;
 		this.placed_feature = placed_feature;
 		this.tree_definition = new Tree_definition(data);
+		this.oreFeature = new OreFeature(data);
 		String[] allFiles = this.data.parser.worldgen.configured_feature.getAllFiles();
 		for (String file : allFiles) {
 			JSONObject json = this.data.parser.worldgen.configured_feature.getFile(file);
@@ -66,192 +68,191 @@ public final class Configured_feature {
 
 		switch (type) {
 			case "minecraft:bamboo":
-				System.out.println("bamboo is not implemented yet");
+				//System.out.println("bamboo is not implemented yet");
 				return null;
 			case "minecraft:basalt_columns":
-				System.out.println("basalt_columns is not implemented yet");
+				//System.out.println("basalt_columns is not implemented yet");
 				return null;
 			case "minecraft:block_column":
-				System.out.println("block_column is not implemented yet");
+				//System.out.println("block_column is not implemented yet");
 				return null;
 			case "minecraft:block_pile":
-				System.out.println("block_pile is not implemented yet");
+				//System.out.println("block_pile is not implemented yet");
 				return null;
 			case "minecraft:delta_feature":
-				System.out.println("delta_feature is not implemented yet");
+				//System.out.println("delta_feature is not implemented yet");
 				return null;
 			case "minecraft:disk":
-				System.out.println("disk is not implemented yet");
+				//System.out.println("disk is not implemented yet");
 				return null;
 			case "minecraft:dripstone_cluster":
-				System.out.println("dripstone_cluster is not implemented yet");
+				//System.out.println("dripstone_cluster is not implemented yet");
 				return null;
 			case "minecraft:end_gateway":
-				System.out.println("end_gateway is not implemented yet");
+				//System.out.println("end_gateway is not implemented yet");
 				return null;
 			case "minecraft:end_spike":
-				System.out.println("end_spike is not implemented yet");
+				//System.out.println("end_spike is not implemented yet");
 				return null;
 			case "minecraft:fill_layer":
-				System.out.println("fill_layer is not implemented yet");
+				//System.out.println("fill_layer is not implemented yet");
 				return null;
 			case "minecraft:flower":
 				return flower(config);
 			case "minecraft:fallen_tree":
-				System.out.println("fallen_tree is not implemented yet");
+				//System.out.println("fallen_tree is not implemented yet");
 				return null;
 			case "minecraft:forest_rock":
-				System.out.println("forest_rock is not implemented yet");
+				//System.out.println("forest_rock is not implemented yet");
 				return null;
 			case "minecraft:fossil":
-				System.out.println("fossil is not implemented yet");
+				//System.out.println("fossil is not implemented yet");
 				return null;
 			case "minecraft:geode":
-				System.out.println("geode is not implemented yet");
+				//System.out.println("geode is not implemented yet");
 				return null;
 			case "minecraft:huge_brown_mushroom":
-				System.out.println("huge_brown_mushroom is not implemented yet");
+				//System.out.println("huge_brown_mushroom is not implemented yet");
 				return null;
 			case "minecraft:huge_fungus":
-				System.out.println("huge_fungus is not implemented yet");
+				//System.out.println("huge_fungus is not implemented yet");
 				return null;
 			case "minecraft:huge_red_mushroom":
-				System.out.println("huge_red_mushroom is not implemented yet");
+				//System.out.println("huge_red_mushroom is not implemented yet");
 				return null;
 			case "minecraft:iceberg":
-				System.out.println("iceberg is not implemented yet");
+				//System.out.println("iceberg is not implemented yet");
 				return null;
 			case "minecraft:lake":
-				System.out.println("lake is not implemented yet");
+				//System.out.println("lake is not implemented yet");
 				return null;
 			case "minecraft:large_dripstone":
-				System.out.println("large_dripstone is not implemented yet");
+				//System.out.println("large_dripstone is not implemented yet");
 				return null;
 			case "minecraft:multiface_growth":
-				System.out.println("multiface_growth is not implemented yet");
+				//System.out.println("multiface_growth is not implemented yet");
 				return null;
 			case "minecraft:nether_forest_vegetation":
-				System.out.println("nether_forest_vegetation is not implemented yet");
+				//System.out.println("nether_forest_vegetation is not implemented yet");
 				return null;
 			case "minecraft:netherrack_replace_blobs":
-				System.out.println("netherrack_replace_blobs is not implemented yet");
+				//System.out.println("netherrack_replace_blobs is not implemented yet");
 				return null;
 			case "minecraft:no_bonemeal_flower":
-				System.out.println("no_bonemeal_flower is not implemented yet");
+				//System.out.println("no_bonemeal_flower is not implemented yet");
 				return null;
 			case "minecraft:ore":
-				System.out.println("ore is not implemented yet");
-				return null;
+				return this.oreFeature.parse(config);
 			case "minecraft:pointed_dripstone":
-				System.out.println("pointed_dripstone is not implemented yet");
+				//System.out.println("pointed_dripstone is not implemented yet");
 				return null;
 			case "minecraft:random_boolean_selector":
-				System.out.println("random_boolean_selector is not implemented yet");
+				//System.out.println("random_boolean_selector is not implemented yet");
 				return null;
 			case "minecraft:random_selector":
 				return random_selector(config);
 			case "minecraft:random_patch":
 				return flower(config);
 			case "minecraft:replace_single_block":
-				System.out.println("replace_single_block is not implemented yet");
+				//System.out.println("replace_single_block is not implemented yet");
 				return null;
 			case "minecraft:root_system":
-				System.out.println("root_system is not implemented yet");
+				//System.out.println("root_system is not implemented yet");
 				return null;
 			case "minecraft:scattered_ore":
-				System.out.println("scattered_ore is not implemented yet");
+				//System.out.println("scattered_ore is not implemented yet");
 				return null;
 			case "minecraft:sculk_patch":
-				System.out.println("sculk_patch is not implemented yet");
+				//System.out.println("sculk_patch is not implemented yet");
 				return null;
 			case "minecraft:seagrass":
-				System.out.println("seagrass is not implemented yet");
+				//System.out.println("seagrass is not implemented yet");
 				return null;
 			case "minecraft:sea_pickle":
-				System.out.println("sea_pickle is not implemented yet");
+				//System.out.println("sea_pickle is not implemented yet");
 				return null;
 			case "minecraft:simple_block":
 				return simple_block(config);
 			case "minecraft:simple_random_selector":
 				return simple_random_selector(config);
 			case "minecraft:spike":
-				System.out.println("spike is not implemented yet");
+				//System.out.println("spike is not implemented yet");
 				return null;
 			case "minecraft:spring_feature":
-				System.out.println("spring_feature is not implemented yet");
+				//System.out.println("spring_feature is not implemented yet");
 				return null;
 			case "minecraft:tree":
 				return this.tree_definition.parse(config);
 			case "minecraft:twisting_vines":
-				System.out.println("twisting_vines is not implemented yet");
+				//System.out.println("twisting_vines is not implemented yet");
 				return null;
 			case "minecraft:underwater_magma":
-				System.out.println("underwater_magma is not implemented yet");
+				//System.out.println("underwater_magma is not implemented yet");
 				return null;
 			case "minecraft:vegetation_patch":
-				System.out.println("vegetation_patch is not implemented yet");
+				//System.out.println("vegetation_patch is not implemented yet");
 				return null;
 			case "minecraft:waterlogged_vegetation_patch":
-				System.out.println("waterlogged_vegetation_patch is not implemented yet");
+				//System.out.println("waterlogged_vegetation_patch is not implemented yet");
 				return null;
 			// Configuration-less features
 			case "minecraft:basalt_pillar":
-				System.out.println("basalt_pillar is not implemented yet");
+				//System.out.println("basalt_pillar is not implemented yet");
 				return null;
 			case "minecraft:blue_ice":
-				System.out.println("blue_ice is not implemented yet");
+				//System.out.println("blue_ice is not implemented yet");
 				return null;
 			case "minecraft:bonus_chest":
-				System.out.println("bonus_chest is not implemented yet");
+				//System.out.println("bonus_chest is not implemented yet");
 				return null;
 			case "minecraft:chorus_plant":
-				System.out.println("chorus_plant is not implemented yet");
+				//System.out.println("chorus_plant is not implemented yet");
 				return null;
 			case "minecraft:coral_claw":
-				System.out.println("coral_claw is not implemented yet");
+				//System.out.println("coral_claw is not implemented yet");
 				return null;
 			case "minecraft:coral_mushroom":
-				System.out.println("coral_mushroom is not implemented yet");
+				//System.out.println("coral_mushroom is not implemented yet");
 				return null;
 			case "minecraft:coral_tree":
-				System.out.println("coral_tree is not implemented yet");
+				//System.out.println("coral_tree is not implemented yet");
 				return null;
 			case "minecraft:desert_well":
-				System.out.println("desert_well is not implemented yet");
+				//System.out.println("desert_well is not implemented yet");
 				return null;
 			case "minecraft:end_island":
-				System.out.println("end_island is not implemented yet");
+				//System.out.println("end_island is not implemented yet");
 				return null;
 			case "minecraft:end_platform":
-				System.out.println("end_platform is not implemented yet");
+				//System.out.println("end_platform is not implemented yet");
 				return null;
 			case "minecraft:freeze_top_layer":
-				System.out.println("freeze_top_layer is not implemented yet");
+				//System.out.println("freeze_top_layer is not implemented yet");
 				return null;
 			case "minecraft:glowstone_blob":
-				System.out.println("glowstone_blob is not implemented yet");
+				//System.out.println("glowstone_blob is not implemented yet");
 				return null;
 			case "minecraft:kelp":
-				System.out.println("kelp is not implemented yet");
+				//System.out.println("kelp is not implemented yet");
 				return null;
 			case "minecraft:monster_room":
-				System.out.println("monster_room is not implemented yet");
+				//System.out.println("monster_room is not implemented yet");
 				return null;
 			case "minecraft:no_op":
-				System.out.println("no_op is not implemented yet");
+				//System.out.println("no_op is not implemented yet");
 				return null;
 			case "minecraft:vines":
-				System.out.println("vines is not implemented yet");
+				//System.out.println("vines is not implemented yet");
 				return null;
 			case "minecraft:void_start_platform":
-				System.out.println("void_start_platform is not implemented yet");
+				//System.out.println("void_start_platform is not implemented yet");
 				return null;
 			case "minecraft:weeping_vines":
-				System.out.println("weeping_vines is not implemented yet");
+				//System.out.println("weeping_vines is not implemented yet");
 				return null;
 			default:
 				//throw new RuntimeException("Unsupported configured feature type: " + type + " in config: " + json);
-				System.out.println("Unsupported configured feature type: " + type + " in config: " + json);
+				//System.out.println("Unsupported configured feature type: " + type + " in config: " + json);
 				return null;
 		}
 	}
@@ -311,7 +312,7 @@ public final class Configured_feature {
 				BlockState state = BlockStateProvider.getBlockState(this.data, to_place, x, y, z);
 				Integer id = Registry.getId(state.identifier);
 				if (id == null) {
-					System.out.println("Could not find block id for simple_block with state: " + state.identifier);
+					//System.out.println("Could not find block id for simple_block with state: " + state.identifier);
 					return null;
 				}
 				return List.of(new Configured_featureInfo(x, y, z, id, true));
